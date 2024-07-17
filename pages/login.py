be7f287e732bb5516f0a2dada722b56906139653
marina <marinaemad559@gmail.com>
2024-07-17 23:51:00 +0300

    create test config, add page model for the login page, and add first tests for the login page(test_successful_login)

--- pages/login.py
+++ pages/login.py
@@ -1,15 +1,13 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-
 
 class LoginPage:
     # URL of the login page
-    baseURL = 'https://www.saucedemo.com/'
+    base_url = 'https://www.saucedemo.com/'
 
     # Locators for elements on the login page
-    username = (By.ID, 'user-name')
-    password = (By.ID, 'password')
-    login_button = (By.ID, 'login-button')
+    username_locator = (By.ID, 'user-name')
+    password_locator = (By.ID, 'password')
+    login_button_locator = (By.ID, 'login-button')
 
     # Constructor to initialize the class with a browser instance
     def __init__(self, browser):
@@ -17,18 +15,27 @@ class LoginPage:
 
     # Method to load the login page
     def load(self):
-        self.browser.get(self.baseURL)
+        self.browser.get(self.base_url)
+
+    # Method to enter username in the username input field
+    def enter_username(self, username):
+        username_input = self.browser.find_element(*self.username_locator)
+        username_input.clear()  # Clear any existing text in the field
+        username_input.send_keys(username)
+
+    # Method to enter password in the password input field
+    def enter_password(self, password):
+        password_input = self.browser.find_element(*self.password_locator)
+        password_input.clear()  # Clear any existing text in the field
+        password_input.send_keys(password)
+
+    # Method to click the login button
+    def click_login_button(self):
+        login_button = self.browser.find_element(*self.login_button_locator)
+        login_button.click()
 
     # Method to perform login with provided credentials
     def login(self, username, password):
-        # Locate the username input field and enter the provided username
-        username_input = self.browser.find_element(*self.username)
-        username_input.send_keys(username)
-
-        # Locate the password input field and enter the provided password
-        password_input = self.browser.find_element(*self.password)
-        password_input.send_keys(password)
-
-        # Locate the login button and click on it to submit the login form
-        login_button = self.browser.find_element(*self.login_button)
-        login_button.click()
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login_button()
